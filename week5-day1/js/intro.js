ae87fe4edb1fd9a659e170ee8d72fed81f3a33aa
Daniel Yuschick <danyuschick@gmail.com>
2017-02-06 12:40:40 -0500

    Updates to Week 5 Day 1 in class

---
+++ week5-day1/js/intro.js
@@ -0,0 +1,30 @@
+(function() {
+    "use strict";
+
+    // document.querySelector('.test-section').classList.add('active');
+    // $('.test-section').addClass('active');
+    // $('input[type=text], p, .test-section').addClass('active');
+    //
+    // // document.querySelector('.test-section').classList.remove('active');
+    // $('.test-section').removeClass('active');
+    //
+    // // document.querySelector('.test-section').classList.toggle('active');
+    // $('.test-section').toggleClass('active');
+
+    $(document).ready(() => {
+        // $('nav li').eq(2).toggleClass('active');
+
+        // $('a[href^="http"]').css({
+        //     color: 'red',
+        //     textDecoration: 'none'
+        // });
+
+        // $('nav li.active').siblings().toggle();
+
+        // $('.all-dogs-container').parents('main').css('background', 'red');
+        // $('.all-dogs-container').children('.dog-container').css('background', 'blue');
+
+        // console.log($('.dog-container.abbie').attr('data-name', 'another'));
+
+    });
+})();
